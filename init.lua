bfca60c738bca7451690c0903c19a0280de526d7
scaamano <scaamanho@gmail.com>
2018-02-16 00:32:50 +0100

    antes de quitar comprobaciones

--- init.lua
+++ init.lua
@@ -8,45 +8,54 @@ local modpath = minetest.get_modpath(minetest.get_current_modname())
 
 ambilight = {}
 
-function ambilight.register_light2(name, node)
+function ambilight.register_light(name, node)
 	if not node.drawtype then
 		node.drawtype="nodebox"
 	end
 
 	if not node.tiles then
 		if not node.id then
-			--load default texture
+			-- default texture
+			node.tiles = {"ambilight_light1.png"} 
 		else
+			-- ambilight predefined id
 			node.tiles = {"ambilight_light"..node.id..".png"}
 		end
 	end
 
 	if not node.inventory_image then
 		if not node.id then
-			--load default texture
+			--defaut inv img
+			node.inventory_image = node.tiles 
 		else
+			-- ambilight predefined id
 			node.inventory_image = {"ambilight_light"..node.id..".png"}
 		end
 	end
 
 	if not node.wield_image then
 		if not node.id then
-			--load default texture
+			-- default weild image
+			node.wield_image = node.tiles 
 		else
-			node.wield_image = {"ambilight_light"..node.id..".png"}
+			-- ambilight predefined id
+			node.wield_image = {"ambilight_light"..node.id..".png"} -- ambilight predefined
 		end
 	end
 
+	-- set default node description
 	if not node.description then
 		node.description = "Ambilight "..name
 	end
 
+	-- set default light intensity
 	if not node.light_source then
 		node.light_source = default.LIGHT_MAX - 5
 	end
 
+	-- set light interaction
 	if not node.enable_interact then
-		--do nothing
+		node.enable_interact = false
 	else
 		node.on_punch = function(pos, node, clicker, itemstack, pointed_thing)
 			minetest.set_node(pos, {name = "ambilight:"..name.."_off"})
@@ -70,13 +79,15 @@ function ambilight.register_light2(name, node)
 	})
 
 
-	if not node.enable_interact then
-	else
+	-- set light off
+	if node.enable_interact then
 
 		if not node.tiles_off then
 			if not node.id then
 				--load default texture
+				node.tiles_off = node.tiles
 			else
+				-- ambilight predefined id
 				node.tiles_off = {"ambilight_light"..node.id.."_off.png"}
 			end
 		end
@@ -100,73 +111,59 @@ function ambilight.register_light2(name, node)
 end
 
 
-
-function ambilight.register_light(id, light_level, drawtype, selection_box, tiles)
-	if not drawtype then
-		drawtype = "nodebox"
-	end	
-
-	if not tiles then
-		tiles = {"ambilight_light"..id..".png"}
-	else 
-		tiles=tiles
-	end
-
-	minetest.register_node("ambilight:"..id, {
-		description = "Light "..id.." test",
-		drawtype = drawtype,
-		selection_box = selection_box,
-		tiles = tiles,
-		inventory_image = "ambilight_light"..id..".png",
-		wield_image = "ambilight_light"..id..".png",
-		paramtype = "light",
-		light_source = light_level,
-		
-		--buildable_to = true,
-		on_punch = function(pos, node, clicker, itemstack, pointed_thing)
-			minetest.set_node(pos, {name = "ambilight:"..id.."_off"})	  
-		end,
-		groups = {dig_immediate=2},
-		sounds = default.node_sound_glass_defaults(),
-	})
-	
-
-	if not tiles then
-		tiles = {"ambilight_light"..id.."_off.png"}
-	else 
-		tiles=tiles
-	end
-
-	minetest.register_node("ambilight:"..id.."_off", {
-		description = "Light "..id.." test off",
-		drawtype = drawtype,
-		selection_box = selection_box,
-		tiles = tiles,
-		wield_image = "ambilight_light"..id..".png",
-		paramtype = "light",
-		--buildable_to = true,
-		groups = {dig_immediate=2, not_in_creative_inventory=1},
-		on_punch = function(pos, node, clicker, itemstack, pointed_thing)
-			minetest.set_node(pos, {name = "ambilight:"..id})	  
-		end,
-		drop = "ambilight:"..id
-		
-	})
-end
-
-ambilight.register_light2("one",{
+-- default mod lights
+ambilight.register_light("one",{
 	id=1,
 	enable_interact=true
 })
---ambilight.register_light(1,default.LIGHT_MAX-4)
-ambilight.register_light(2,default.LIGHT_MAX-4,"nodebox",{})
-ambilight.register_light(3,default.LIGHT_MAX-4,"nodebox",{})
-ambilight.register_light(4,default.LIGHT_MAX-4)
-ambilight.register_light(5,default.LIGHT_MAX-4)
-ambilight.register_light(6,default.LIGHT_MAX-4)
-ambilight.register_light(7,default.LIGHT_MAX-4,"plantlike",{
+
+ambilight.register_light("two",{
+	id=2,
+	light_source = default.LIGHT_MAX-4,
+	enable_interact=true
+})
+
+ambilight.register_light("tree",{
+	id=3,
+	light_source = default.LIGHT_MAX-4,
+	enable_interact=true
+})
+
+ambilight.register_light("four",{
+	id=4,
+	light_source = default.LIGHT_MAX-4,
+	enable_interact=true
+})
+
+ambilight.register_light("five",{
+	id=5,
+	light_source = default.LIGHT_MAX-4,
+	enable_interact=true
+})
+
+ambilight.register_light("six",{
+	id=6,
+	light_source = default.LIGHT_MAX-4,
+	enable_interact=true
+})
+
+ambilight.register_light("seven",{
+	id=7,
+	light_source = default.LIGHT_MAX-4,
+	drawtype ="plantlike",
 	type = "fixed",
-	fixed = {-0.25, -0.5, -0.25, 0.25, 0.4, 0.25}
+	fixed = {-0.25, -0.5, -0.25, 0.25, 0.4, 0.25},
+	enable_interact=true
+})
+
+ambilight.register_light("petrol3",{
+	light_source = default.LIGHT_MAX-4,
+	drawtype ="plantlike",
+	type = "fixed",
+	fixed = {-0.25, -0.5, -0.25, 0.25, 0.4, 0.25},
+	tiles = {"ambitest3_32.png"},
+	inventory_image = "ambitest3_32.png",
+	wield_image = "ambitest3_32.png"
 })
 
 --ambilight.register_light(8,default.LIGHT_MAX-1,nil,nil,{"ambilight_light8.png"})
@@ -177,8 +174,8 @@ minetest.register_node("ambilight:window1", {
 	description = "Light 6 test",
 	drawtype = "nodebox",
 	tiles = {"ambilight_window1.png"},
-	inventory_image = "ambilight_window1.png",
-	wield_image = "ambilight_window1.png",
+--	inventory_image = "ambilight_window1.png",
+--	wield_image = "ambilight_window1.png",
 	paramtype = "light",
 	light_source = 5,
 	--buildable_to = true,
@@ -190,8 +187,8 @@ minetest.register_node("ambilight:window2", {
 	description = "Light 6 test",
 	drawtype = "nodebox",
 	tiles = {"ambilight_window2.png"},
-	inventory_image = "ambilight_window2.png",
-	wield_image = "ambilight_window2.png",
+--	inventory_image = "ambilight_window2.png",
+--	wield_image = "ambilight_window2.png",
 	paramtype = "light",
 	light_source = 5,
 	--buildable_to = true,
